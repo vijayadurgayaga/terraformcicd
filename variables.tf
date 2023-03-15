033ad755f39b31e806a90453087c9bea2f3b6d3d
vijaya <vijayadurgayaga888@gmil.com>
2023-03-15 16:40:33 +0530

    vatriblefile

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "subscription_id" {
+    type = string
+    default = "3e6843a7-1cf3-4646-8f11-9cc4cc2f7a9"
+    description = "newsubscription_id" 
+}
+variable "client_id" {
+    type = string
+    default = "efeeec21-fe41-4e31-9e1e-bb47b0ef7155"
+    description = "newclient_id"
+  
+}
+variable "client_secret" {
+    type = string
+    default = "qUi8Q~LUK5fXHYFHUyaucJGPAxxOySvhI1vrucSv"
+    description = "newclient_secrete"
+  
+}
+variable "tenant_id" {
+    type = string
+    default = "6e4db550-dfaf-4282-a090-18af3da906a1"
+    description = "newtenant_id"
+  
+}
